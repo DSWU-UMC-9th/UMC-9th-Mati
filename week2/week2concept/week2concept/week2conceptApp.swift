4efce0095de390b5228f38d804e649e24c987c6e
alwn8918 <alwn8918@gmail.com>
2025-09-24 19:20:47 +0900

    ✨ [week2] 개념: @State와 @Binding의 사용 익히기

---
+++ week2/week2concept/week2concept/week2conceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  week2conceptApp.swift
+//  week2concept
+//
+//  Created by 김미주 on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct week2conceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            StateBindingView()
+        }
+    }
+}
