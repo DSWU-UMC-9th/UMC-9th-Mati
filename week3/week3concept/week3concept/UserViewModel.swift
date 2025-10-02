2b89e88c02fa7cc840167b6a5f8dca7a795d4e60
alwn8918 <alwn8918@gmail.com>
2025-10-03 01:56:31 +0900

    ✨ [week3] 개념: Identifiable 프로토콜을 활용한 데이터 바인딩

---
+++ week3/week3concept/week3concept/UserViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  UserViewModel.swift
+//  week3concept
+//
+//  Created by 김미주 on 10/3/25.
+//
+
+import Foundation
+
+@Observable
+class UserViewModel {
+    var users = [
+        User(name: "Alice", age: 24),
+        User(name: "Bob", age: 30),
+        User(name: "Charlie", age: 28)
+    ]
+}
